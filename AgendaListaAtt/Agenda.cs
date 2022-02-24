05840e62246f16ec5395be479d8646a280a3aea1
serranomatheus <serranomatheus2@icloud.com>
2022-02-23 21:51:08 -0300

    a

--- AgendaListaAtt/Agenda.cs
+++ AgendaListaAtt/Agenda.cs
@@ -17,6 +17,33 @@ namespace AgendaListaAtt
             Head = null;
             Tail = null;
         }
+
+        public void Pop(Contato contato)
+        {
+            if(contato.Nome == Head.Nome)
+            {
+                Head = Head.Proximo;
+            }
+            else
+            {
+                Contato aux = Head;
+                Contato aux1 = Head.Proximo;
+                for(int i = 0; i < Quantidade; i++)
+                {
+                    if(string.Compare(contato.Nome, aux1.Nome) == 0)
+                    {
+                        aux.Proximo = aux1.Proximo;
+                        if(aux1.Proximo == null)
+                        {
+                            Tail = aux;
+                        }
+                    }
+                    else
+                        aux = aux1;
+                        aux1 = aux1.Proximo;
+                }
+            }
+        }
         public bool Vazio()
         {
             if (Head == null & Tail == null)
@@ -45,23 +72,78 @@ namespace AgendaListaAtt
         }
         public void Print()
         {
+            Console.Clear();
             if (Vazio())
             {
-                Console.WriteLine("Agenda vazia");
+                Console.WriteLine("************** Agenda vazia ****************");
             }
             else
             {
+
                 Contato aux = Head;
                 do
                 {
+                    Console.WriteLine("--------------------------------------------");
                     Console.WriteLine(aux.ToString());
                     aux = aux.Proximo;
+                    Console.ReadKey();
                 } while (aux != null);
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("********* Fim da lista de Contatos *********");
             }
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
+            Console.WriteLine("--------------------------------------------");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+        public void BuscarPop()
+        {
+            if (Vazio())
+            {
+                Console.WriteLine("************** Agenda vazia ****************");
+            }
+        }
+        public void Buscar()
+        {
+            Console.Clear();
+            Contato aux = Head;
+            bool cont = false;
+            Console.Clear();
+            if (Vazio())
+            {
+                Console.WriteLine("************** Agenda vazia ****************");
+            }
+            else
+            {
+                Console.WriteLine("Qual nome deseja buscar?");
+                string nomeBuscar = Console.ReadLine();
+                do
+                {
+                    if (aux.Nome.ToLower().Contains(nomeBuscar))
+                    {
+                        Console.WriteLine("--------------------------------------------");
+                        Console.WriteLine(aux.ToString());
+                        cont = true;
+                    }
+                    aux = aux.Proximo;
+                } while (aux != null);
+
+                if (!cont)
+                {
+                    Console.WriteLine("*******  Nenhum Contato encontrado!! *******");
+                }               
+            }
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
+            Console.WriteLine("--------------------------------------------");
+            Console.ReadKey();
+            Console.Clear();
         }
         public void Sort()
         {
-            Contato aux = new Contato(null,null,null);
+            Contato aux = new Contato(null, null, null);
             for (Contato referencia = Head; referencia != null; referencia = referencia.Proximo)
             {
                 for (Contato comparacao = referencia.Proximo; comparacao != null; comparacao = comparacao.Proximo)
@@ -75,12 +157,12 @@ namespace AgendaListaAtt
                         referencia.Nome = comparacao.Nome;
                         referencia.Email = comparacao.Email;
                         referencia.Telefones = comparacao.Telefones;
-                      //  Console.WriteLine(referencia.ToString());
+                        //  Console.WriteLine(referencia.ToString());
                         comparacao.Nome = aux.Nome;
                         comparacao.Email = aux.Email;
                         comparacao.Telefones = aux.Telefones;
-                      //  Console.WriteLine(comparacao.ToString());
-                        
+                        //  Console.WriteLine(comparacao.ToString());
+
                     }
                 }
             }
--- AgendaListaAtt/Program.cs
+++ AgendaListaAtt/Program.cs
@@ -10,23 +10,72 @@ namespace AgendaListaAtt
             string opc = "a";
             do
             {
-                Console.WriteLine("Nome");
+                Console.Clear();
+                opc = Menu();
+
+                switch (opc)
+                {
+                    case "1": InserirContato();
+                        break;
+                    case "2":
+                        break;
+                    case "3":agenda.Buscar();
+                        break;
+                    case "4": agenda.Print();
+                        break;
+                    case "5":
+                        break;
+                    case "0":
+                        break;
+                            default:
+                        Console.Clear();
+                        Console.WriteLine("*******  Nenhuma opcao selecionado  ********");
+                        Console.WriteLine("--------------------------------------------");
+                        Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
+                        Console.WriteLine("--------------------------------------------");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                }
+            }while (opc != "0");
+
+            void Buscar()
+            {
+                Console.Clear();
+                if (agenda.Vazio())
+                {
+
+                }
+            }
+
+            void InserirContato()
+            {
+                Console.Write("Nome: ");
                 string nome = Console.ReadLine();
-                Console.WriteLine("Email");
+                Console.Write("Email: ");
                 string email = Console.ReadLine();
-                Console.WriteLine("Tipo");
+                Console.Write("Tipo: ");
                 string tipo = Console.ReadLine();
-                Console.WriteLine("Ddd");
+                Console.Write("Ddd: ");
                 string ddd = Console.ReadLine();
-                Console.WriteLine("Numero");
+                Console.Write("Numero: ");
                 string numero = Console.ReadLine();
-                Console.WriteLine("sair");
-                opc = Console.ReadLine();
                 agenda.Push(new Contato(nome, email, new Telefone(tipo, ddd, numero)));
-            } while (opc != "0");
-            agenda.Print();
-        
-        
+            }          
+
+            string Menu()
+            {
+                Console.WriteLine("***** MENU *****");
+                Console.WriteLine("[1]Adicionar Contato");
+                Console.WriteLine("[2]Remover Contato");
+                Console.WriteLine("[3]Buscar Contato");
+                Console.WriteLine("[4]Imprimir Contato");
+                Console.WriteLine("[5]Editar Contato");
+                Console.WriteLine("[0]Sair");
+                return Console.ReadLine();
+            }
+
         }
     }
 }
